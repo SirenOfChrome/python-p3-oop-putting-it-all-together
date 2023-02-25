f9a3a068f766f2c0ab147978f4c2d14319d2c3e3
Forrest Jones <forrestjones2010@gmail.com>
2023-02-25 10:00:02 -0600

    GDC#3

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+        self.author = None
+        self.page_count = None
+        self.genre = None
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
